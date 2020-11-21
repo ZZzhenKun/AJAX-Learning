55e5b802135b8ddcf8c59470b43e5734ca88d647
ZZK <1042134104@qq.com>
2020-11-21 21:59:34 +0800

    11.21

--- 代码/3-原生AJAX/server.js
+++ 代码/3-原生AJAX/server.js
@@ -6,38 +6,54 @@ const app = express();
 // 3.创建路由规则
 // request 是对请求报文的封装
 // response 是对响应报文的封装
-app.get('/server', (request, response)=>{
+app.get('/server', (request, response) => {
     // 设置响应头 设置允许跨域
-    response.setHeader('Access-Control-Allow-Origin','*');
+    response.setHeader('Access-Control-Allow-Origin', '*');
     // 设置响应体
     response.send("HELLO AJAX !");
 });
+
 // 可以接收任意类型的请求
-app.all('/server', (request, response)=>{
+app.all('/server', (request, response) => {
     // 设置响应头 设置允许跨域
-    response.setHeader('Access-Control-Allow-Origin','*');
+    response.setHeader('Access-Control-Allow-Origin', '*');
     // 响应头
-    response.setHeader('Access-Control-Allow-Headers','*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
     // 设置响应体
     response.send("HELLO AJAX POST!");
 });
 
-
-app.all('/json-server', (request, response)=>{
+app.all('/json-server', (request, response) => {
     // 设置响应头 设置允许跨域
-    response.setHeader('Access-Control-Allow-Origin','*');
+    response.setHeader('Access-Control-Allow-Origin', '*');
     // 响应头
-    response.setHeader('Access-Control-Allow-Headers','*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
     // 响应一个数据
     const data = {
-        name:'cqupt'
+        name: 'cqupt -3'
     }
     // 对对象进行字符后转换
     let str = JSON.stringify(data);
     // 设置响应体
     response.send(str);
 });
+
+// 针对IE缓存
+// app.get('/ie',function(request,response){
+//     // 设置响应头 设置允许跨域
+//     response.setHeader('Access-Control-Allow-Origin','*');
+//     // 设置响应体
+//     response.send("HELLO IE 9!");
+// }); 
+//上面写法等价于下面的写法，
+app.get('/ie', (request, response) => {
+    // 设置响应头 设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    // 设置响应体
+    response.send("HELLO IE 7!");
+});
+    
 // 4.监听端口启动服务
-app.listen(8000, ()=>{
+app.listen(8000, () => {
     console.log("服务已经启动， 8000 端口监听中.....");
-})
+})           
