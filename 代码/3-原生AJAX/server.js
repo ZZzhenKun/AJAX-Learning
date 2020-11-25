9e45b67b03556f9f15753a03aff3e663d019f1b9
ZZK <1042134104@qq.com>
2020-11-25 21:14:15 +0800

    11.25

--- 代码/3-原生AJAX/server.js
+++ 代码/3-原生AJAX/server.js
@@ -52,7 +52,20 @@ app.get('/ie', (request, response) => {
     // 设置响应体
     response.send("HELLO IE 7!");
 });
-    
+
+// 延迟响应
+app.get('/delay', (request, response) => {
+    // 设置响应头 设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    // 设置响应体
+    setTimeout(() => {
+        response.send("延迟响应");
+    }, 3000);
+    // setTimeout (function(){
+    //     response.send("延迟响应");
+    // },2000);
+
+});
 // 4.监听端口启动服务
 app.listen(8000, () => {
     console.log("服务已经启动， 8000 端口监听中.....");
